278704d554fadc502366924a7203dbf5a4453c09
mark3317 <mark.nokhrin@gmail.com>
2023-04-11 14:34:11 +0300

    application 5_IIR

--- src/equalizer/Filter.java
+++ src/equalizer/Filter.java
@@ -5,13 +5,17 @@ import java.util.concurrent.Callable;
 public class Filter implements Callable<short[]> {
     private short[] inputSignal;
     private short[] outputSignal;
+    private double[] feedbackSignal;
     private double gain;
     private double[] coffsNumFilter;
+    private double[] coffsDenFilter;
 
-    public void settings(final short[] inputSignal, final double[] coffsNumFilter) {
+    public void settings(final short[] inputSignal, final double[] coffsNumFilter, final double[] coffsDenFilter) {
         this.inputSignal = inputSignal;
         this.coffsNumFilter = coffsNumFilter;
+        this.coffsDenFilter = coffsDenFilter;
         this.outputSignal = new short[inputSignal.length];
+        this.feedbackSignal =  new double[inputSignal.length];
     }
 
     private void convolution() {
@@ -19,9 +23,12 @@ public class Filter implements Callable<short[]> {
         for(int i = 0; i <  this.inputSignal.length; i++) {
             tmp = 0;
             for(int j = 0; j < FilterInfo.COUNT_OF_COFFS; j++) {
-                if(i - j >= 0)
-                    tmp += coffsNumFilter[j] * this.inputSignal[i - j];
+                if(i - j >= 0){
+                    tmp += this.coffsNumFilter[j] * this.inputSignal[i - j];
+                    tmp -= this.coffsDenFilter[j] * this.feedbackSignal[i - j];
+                }
             }
+            this.feedbackSignal[i] = tmp;
             this.outputSignal[i] += this.gain * (short)(tmp / 4); //делим на 4, чтобы не было перегруза на пересечении фильтров
         }
     }
